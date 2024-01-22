40c32846442055fd0fb5faf468b8a8e7da6a639c
Santos Reyes <sreyesx23@gmail.com>
2024-01-22 11:41:03 -0500

    Add basic logic to function and test.

--- app.js
+++ app.js
@@ -27,3 +27,26 @@ console.log(getComputerChoice());
     //console.log(choice); */
 
 //console.log();
+
+/* - create a function that plays a round
+   - Said function takes two parameters: playerSelection, computerSelection
+   - Then return a string based on parameters on which is the winner 
+   - Make playerSelection case-insensitive 
+   - If a tie occurs then restart the round */
+
+function playRound(playerSelection, computerSelection) {
+    //getComputerChoice() = computerSelection;
+
+    if(playerSelection > computerSelection) {
+        return 'You win!'
+    }
+    else if(playerSelection < computerSelection) {
+        return 'You lose! Try again'
+    }
+    else {
+        return 'Tie round! Try again!'
+    }
+}
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
