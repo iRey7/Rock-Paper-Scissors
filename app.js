8db70bebc9569bfe2084a00980f75cbcc253c14a
Santos Reyes <sreyesx23@gmail.com>
2024-01-29 11:27:55 -0500

    Clean out some comments.

--- app.js
+++ app.js
@@ -12,31 +12,9 @@ function getComputerChoice() {
     }
     return choice;
 }
-console.log(getComputerChoice());
-/* let number = Math.floor(Math.random() * 3) + 1;
-    //console.log(number);
-    if (number == 1) {
-        choice = 'rock'
-    }
-    else if (number == 2) {
-        choice = 'paper'
-    }
-    else {
-        choice = 'scissors'
-    }
-    //console.log(choice); */
-
-//console.log();
-
-/* - create a function that plays a round
-   - Said function takes two parameters: playerSelection, computerSelection
-   - Then return a string based on parameters on which is the winner 
-   - Make playerSelection case-insensitive 
-   - If a tie occurs then restart the round */
+//console.log(getComputerChoice());
 
 function playRound(playerSelection, computerSelection) {
-    //getComputerChoice() = computerSelection;
-
     if(playerSelection > computerSelection) {
         return 'You win!'
     }
